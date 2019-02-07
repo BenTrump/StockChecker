b1f8f805dc7b647f84729829a6e5d73a435382a8
BenTrump <bmtseducation@gmail.com>
2019-02-06 16:37:10 -0800

    Added check price and check daily plot methods

--- StockChecker.py
+++ StockChecker.py
@@ -1,4 +1,53 @@
 import matplotlib.pyplot as plot
-import fix_yahoo_finance as yf
+import datetime as dt
+from alpha_vantage.timeseries import TimeSeries
+import pandas as pd
 
 
+class Commands:
+    def __init__(self):
+        self.date_today = dt.date.today()
+        self.date_yesterday = dt.date.today() - dt.timedelta(1)
+
+    @staticmethod
+    def check(*args):
+        ts = TimeSeries(key='EJ69MPM068NGTJ30', output_format='pandas')
+        data, meta_data = ts.get_batch_stock_quotes(symbols=args)
+        print(data.head())
+        Calls()
+
+    @staticmethod
+    def daily(ticker):
+        ts = TimeSeries(key='EJ69MPM068NGTJ30', output_format='pandas')
+        data, meta_data = ts.get_intraday(symbol=ticker, interval='1min', outputsize="full")
+        data["4. close"].plot()
+        plot.title("%s closing times, 1 minute interval")
+        plot.grid()
+        plot.show()
+        Calls()
+
+
+class Calls:
+    def __init__(self):
+        self.choose_command()
+
+    @staticmethod
+    def choose_command():
+        print('')
+        command = Commands()
+        call = input("$").split(" ")
+        if call[0] == "check":
+            command.check(', '.join(call[1:]))
+
+        elif call[0] == "daily":
+            command.daily(call[1])
+
+        elif call[0] == "exit":
+            return
+
+        else:
+            print("invalid command")
+            Calls()
+
+
+test = Calls()
