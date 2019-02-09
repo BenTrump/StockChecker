ab40365d369842c83ef429c9a7528af87da10566
BenTrump <bmtseducation@gmail.com>
2019-02-08 16:16:08 -0800

    Added error handling

--- StockChecker.py
+++ StockChecker.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import datetime as dt
 from alpha_vantage.timeseries import TimeSeries
-#  import pandas as pd
 
 
 class Commands:
@@ -11,30 +10,37 @@ class Commands:
 
     @staticmethod
     def check(*args):
-        print(args)
         ts = TimeSeries(key='EJ69MPM068NGTJ30', output_format='pandas')
         data, meta_data = ts.get_batch_stock_quotes(symbols=args)
         print(data.loc[:, ["1. symbol", "2. price", "4. timestamp"]])
         Calls()
 
     def daily(self, ticker):
-        ts = TimeSeries(key='EJ69MPM068NGTJ30', output_format='pandas')
-        data, meta_data = ts.get_intraday(symbol=ticker, interval='1min', outputsize="full")
-        new_data = data.loc[str(self.date_today) + " 09:31:00":, "4. close"]
-        new_data.plot(title="Closing values, 1 min interval")
-        plt.xlabel("Eastern Standard Time")
-        plt.grid()
-        plt.show()
+        try:
+            ts = TimeSeries(key='EJ69MPM068NGTJ30', output_format='pandas')
+            data, meta_data = ts.get_intraday(symbol=ticker, interval='1min', outputsize="full")
+            new_data = data.loc[str(self.date_today) + " 09:31:00":, "4. close"]
+            new_data.plot(title="Closing values, 1 min interval")
+            plt.xlabel("Eastern Standard Time")
+            plt.grid()
+            plt.show()
+        except KeyError:
+            print(ticker.upper() + " is an invalid ticker")
         Calls()
 
     def change(self, *args):
         for ticker in args[0]:
-            ts = TimeSeries(key='EJ69MPM068NGTJ30', output_format='pandas')
-            data, meta_data = ts.get_intraday(symbol=ticker.upper(), interval='1min', outputsize="full")
-            open_price = data.loc[str(self.date_today) + " 09:31:00", "1. open"]
-            close_price = data.iloc[-1, 3]
-            percent_change = (close_price - open_price)/open_price*100
-            print(ticker.upper() + "  " + "%.2f" % percent_change + "%")
+            try:
+                ts = TimeSeries(key='EJ69MPM068NGTJ30', output_format='pandas')
+                data, meta_data = ts.get_intraday(symbol=ticker.upper(), interval='1min', outputsize="full")
+                open_price = data.loc[str(self.date_yesterday) + " 16:00:00", "4. close"]
+                close_price = data.iloc[-1, 3]
+                percent_change = (close_price - open_price)/open_price*100
+                print(ticker.upper() + "  " + "%.2f" % percent_change + "%")
+            except KeyError:
+                print(ticker.upper() + " is an invalid ticker")
+                break
+        Calls()
 
 
 class Calls:
@@ -49,8 +55,12 @@ class Calls:
         if call[0] == "check":
             command.check(', '.join(call[1:]))
 
-        if call[0] == "change":
-            command.change(call[1:])
+        elif call[0] == "change":
+            if len(call[1:]) > 5:
+                print("Alpha Vantage only allows 5 API calls/minute")
+                Calls()
+            else:
+                command.change(call[1:])
 
         elif call[0] == "daily":
             command.daily(call[1])
